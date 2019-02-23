e78b89434196fa20787a8de564ce0711b53138cf
bmcaos <bmartinenac@gmail.com>
2019-02-23 20:22:26 +0000

    Atom beautify

--- main.js
+++ main.js
@@ -1,52 +1,61 @@
-function hslToRgb(h, s, l){
-    var r, g, b;
+function hslToRgb(h, s, l) {
+  var r, g, b;
 
-    if(s == 0){
-        r = g = b = l; // achromatic
-    }else{
-        var hue2rgb = function hue2rgb(p, q, t){
-            if(t < 0) t += 1;
-            if(t > 1) t -= 1;
-            if(t < 1/6) return p + (q - p) * 6 * t;
-            if(t < 1/2) return q;
-            if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
-            return p;
-        }
-
-        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-        var p = 2 * l - q;
-        r = hue2rgb(p, q, h + 1/3);
-        g = hue2rgb(p, q, h);
-        b = hue2rgb(p, q, h - 1/3);
+  if (s == 0) {
+    r = g = b = l; // achromatic
+  } else {
+    var hue2rgb = function hue2rgb(p, q, t) {
+      if (t < 0) t += 1;
+      if (t > 1) t -= 1;
+      if (t < 1 / 6) return p + (q - p) * 6 * t;
+      if (t < 1 / 2) return q;
+      if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+      return p;
     }
 
-    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
-}
-function rgbToHsl(r, g, b){
-    r /= 255, g /= 255, b /= 255;
-    var max = Math.max(r, g, b), min = Math.min(r, g, b);
-    var h, s, l = (max + min) / 2;
+    var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    var p = 2 * l - q;
+    r = hue2rgb(p, q, h + 1 / 3);
+    g = hue2rgb(p, q, h);
+    b = hue2rgb(p, q, h - 1 / 3);
+  }
 
-    if(max == min){
-        h = s = 0; // achromatic
-    }else{
-        var d = max - min;
-        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
-        switch(max){
-            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
-            case g: h = (b - r) / d + 2; break;
-            case b: h = (r - g) / d + 4; break;
-        }
-        h /= 6;
+  return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+}
+
+function rgbToHsl(r, g, b) {
+  r /= 255, g /= 255, b /= 255;
+  var max = Math.max(r, g, b),
+    min = Math.min(r, g, b);
+  var h, s, l = (max + min) / 2;
+
+  if (max == min) {
+    h = s = 0; // achromatic
+  } else {
+    var d = max - min;
+    s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+    switch (max) {
+      case r:
+        h = (g - b) / d + (g < b ? 6 : 0);
+        break;
+      case g:
+        h = (b - r) / d + 2;
+        break;
+      case b:
+        h = (r - g) / d + 4;
+        break;
     }
+    h /= 6;
+  }
 
-    return [Math.round(h*100), Math.round(s*100), Math.round(l*100)];
+  return [Math.round(h * 100), Math.round(s * 100), Math.round(l * 100)];
 }
-function toHexString(number){
+
+function toHexString(number) {
   number < 0 ? number = 0xFFFFFFFF + number + 1 : null;
   a = number.toString(16).toUpperCase();
   a.length == 2 ? a = a : a = '0'.concat(a);
-  return(a);
+  return (a);
 }
 
 function updatecolor(mode) {
@@ -64,12 +73,12 @@ function updatecolor(mode) {
   }
   if (mode == 1) {
     colorhsl = [parseInt(document.getElementById('hueslide').value), parseInt(document.getElementById('satslide').value), parseInt(document.getElementById('lightslide').value)];
-    colorrgb = hslToRgb(colorhsl[0]/100, colorhsl[1]/100, colorhsl[2]/100);
+    colorrgb = hslToRgb(colorhsl[0] / 100, colorhsl[1] / 100, colorhsl[2] / 100);
     colorhex = '#'.concat(toHexString(colorrgb[0]), toHexString(colorrgb[1]), toHexString(colorrgb[2]));
   }
   if (mode == 2) {
     colorhex = document.getElementById('hexvalue').value;
-    colorrgb = [parseInt(colorhex.substr(1,2), 16), parseInt(colorhex.substr(3,4), 16), parseInt(colorhex.substr(5,6), 16)];
+    colorrgb = [parseInt(colorhex.substr(1, 2), 16), parseInt(colorhex.substr(3, 4), 16), parseInt(colorhex.substr(5, 6), 16)];
     colorhsl = rgbToHsl(colorrgb[0], colorrgb[1], colorrgb[2]);
   }
   document.getElementById('huevalue').innerHTML = colorhsl[0];
